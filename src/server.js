a0f90214d8a1f07dc3d58b05ce99fe6e47e97783
Jörn Strömsdörfer <minimalistbear>
2022-01-18 11:09:47 +0100

    console outputs for server.js

--- src/server.js
+++ src/server.js
@@ -20,8 +20,14 @@ app.use(express.json());
 
 // Routes for kuuna Shooter
 app.use('/shooter', express.static(path.join(__dirname, "../public/kuunaShooter")));
-app.get("/shooter/local", (req, res) => { res.sendFile(path.join(__dirname, "../public/kuunaShooter/local/local.html")); } );
-app.get("/shooter/remote", (req, res) => { res.sendFile(path.join(__dirname, "../public/kuunaShooter/remote/remote.html")); } );
+app.get("/shooter/local", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/kuunaShooter/local/local.html"));
+    console.log(new Date().toString() + ": local version of kuuna shooter requested");
+});
+app.get("/shooter/remote", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/kuunaShooter/remote/remote.html"));
+    console.log(new Date().toString() + ": remote version of kuuna shooter requested");
+});
 app.post("/shooter/remote", (req, res) => {
         var socketID = req.body.socketID;
         open('http://localhost:' + port + '/shooter/stream?clientid=' + socketID,{
@@ -33,12 +39,21 @@ app.post("/shooter/remote", (req, res) => {
         res.send('success');
     }
 )
-app.get("/shooter/stream", (req, res) => { res.sendFile(path.join(__dirname, "../public/kuunaShooter/stream/stream.html")); } );
+app.get("/shooter/stream", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/kuunaShooter/stream/stream.html"));
+    console.log(new Date().toString() + ": stream version of kuuna shooter requested");
+});
 
 // Routes for kuuna Jump'N'Run
 app.use('/jumpandrun', express.static(path.join(__dirname, "../public/kuunaJumpAndRun")));
-app.get("/jumpandrun/local", (req, res) => { res.sendFile(path.join(__dirname, "../public/kuunaJumpAndRun/local/local.html")); } );
-app.get("/jumpandrun/remote", (req, res) => { res.sendFile(path.join(__dirname, "../public/kuunaJumpAndRun/remote/remote.html")); } );
+app.get("/jumpandrun/local", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/kuunaJumpAndRun/local/local.html"));
+    console.log(new Date().toString() + ": local version of kuuna jump'n'run requested");
+});
+app.get("/jumpandrun/remote", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/kuunaJumpAndRun/remote/remote.html"));
+    console.log(new Date().toString() + ": remote version of kuuna jump'n'run requested");
+});
 app.post("/jumpandrun/remote", (req, res) => {
         var socketID = req.body.socketID;
         open('http://localhost:' + port + '/jumpandrun/stream?clientid=' + socketID,{
@@ -50,15 +65,22 @@ app.post("/jumpandrun/remote", (req, res) => {
         res.send('success');
     }
 )
-app.get("/jumpandrun/stream", (req, res) => { res.sendFile(path.join(__dirname, "../public/kuunaJumpAndRun/stream/stream.html")); } );
+app.get("/jumpandrun/stream", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/kuunaJumpAndRun/stream/stream.html"));
+    console.log(new Date().toString() + ": stream version of kuuna jump'n'run requested");
+});
 
 /*
  * Setup for socketing
  *
  */
-io.sockets.on("error", e => console.log(e));
+io.sockets.on("error", (e) => {
+    console.log(new Date.toString() + ": " + e);
+});
 
 io.sockets.on("connection", socket => {
+    console.log(new Date().toString() + ": " + socket.id + " has connected");
+
     socket.on("call-client", (data) => {
         socket.to(data.to).emit("client-called", {
             offer: data.offer,
@@ -75,12 +97,14 @@ io.sockets.on("connection", socket => {
 
     socket.on("remote-session-initialised", (data) => {
         socket.to(data.to).emit("remote-session-initialised", { });
+        console.log(new Date().toString() + ": remote session on " + socket.id + " initialised for " + data.to);
     });
 
     socket.on("disconnect", () => {
         io.sockets.emit("client-session-disconnected", {
             socket: socket.id
         });
+        console.log(new Date().toString() + ": " + socket.id + " has disconnected");
     });
 });
 
@@ -89,5 +113,5 @@ io.sockets.on("connection", socket => {
  *
  */
 server.listen(port, () => {
-    console.log(`Server listening at http://localhost:${port}`)
+    console.log(new Date().toString() + `: server listening at http://localhost:${port}`);
 });
