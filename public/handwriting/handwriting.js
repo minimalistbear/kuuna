bcc9f70d6705d466c8e6fa856e605c2311f2b6ef
Jörn Strömsdörfer <minimalistbear>
2022-01-27 12:33:52 +0100

    canvas for handwriting

--- public/handwriting/handwriting.js
+++ public/handwriting/handwriting.js
@@ -1,1 +1,55 @@
-// TODO
+// Constants + global variables
+var canvas = document.getElementById("canvas");
+var context = canvas.getContext("2d");
+var boundings = canvas.getBoundingClientRect();
+
+context.strokeStyle = '#702fa0';
+context.lineWidth = 4;
+
+/*
+ * Mouse Event Handlers
+ *
+ */
+var isDrawing = false;
+var mouseX = 0;
+var mouseY = 0;
+canvas.addEventListener('mousedown', function(event) {
+    setMouseCoordinates(event);
+    isDrawing = true;
+
+    context.beginPath();
+    context.moveTo(mouseX, mouseY);
+});
+canvas.addEventListener('mousemove', function(event) {
+    setMouseCoordinates(event);
+
+    if(isDrawing){
+        context.lineTo(mouseX, mouseY);
+        context.stroke();
+    }
+});
+canvas.addEventListener('mouseup', function(event) {
+    setMouseCoordinates(event);
+    isDrawing = false;
+});
+function setMouseCoordinates(event) {
+    mouseX = event.clientX - boundings.left;
+    mouseY = event.clientY - boundings.top;
+}
+
+/*
+ * Button Handlers
+ *
+ */
+function clearCanvas() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+};
+function downloadAsImage() {
+    var canvasDataURL = canvas.toDataURL();
+    var a = document.createElement('a');
+    a.href = canvasDataURL;
+    a.download = 'writing';
+    a.click();
+
+    clearCanvas();
+};
---
+++ public/handwriting/recogniser.cpp
@@ -0,0 +1,75 @@
+/*
+ * Includes and namespace
+ *
+ */
+#include <emscripten/emscripten.h>
+#include <iostream>
+#include <string>
+
+#include <tesseract/baseapi.h>
+#include <leptonica/allheaders.h>
+
+using namespace std;
+
+/*
+ * Common function declarations:
+ * Exposed to JS
+ *
+ */
+#ifdef __cplusplus
+    extern "C" {
+        #endif
+
+        string EMSCRIPTEN_KEEPALIVE readCharacters(string picPath);
+
+        #ifdef __cplusplus
+    }
+#endif
+
+/*
+ * Common function definitions:
+ * Exposed to JS
+ *
+ */
+#ifdef __cplusplus
+    extern "C" {
+        #endif
+
+        string EMSCRIPTEN_KEEPALIVE readCharacters(string picPath) {
+            char *outText;
+
+            tesseract::TessBaseAPI *api = new tesseract::TessBaseAPI();
+            if (api->Init(NULL, "eng")) {
+                fprintf(stderr, "Could not initialize tesseract.\n");
+                exit(1);
+            }
+
+            Pix *image = pixRead(picPath);
+            api->SetImage(image);
+            
+            outText = api->GetUTF8Text();
+            string outString(outText);
+
+            api->End();
+            delete api;
+            delete [] outText;
+            pixDestroy(&image);
+
+            return outString;
+        }
+
+        #ifdef __cplusplus
+    }
+#endif
+
+/*
+ * Main function
+ * ( To compile code for WebAssembly usage with Emscripten:
+ *   emcc recogniser.cpp -std=c++11 -s WASM=1 -O0 -ltesseract -I<path to tesseract includes> -llept -L<path to leptonica library> -o recogniser.js)
+ *
+ */
+int main() {
+    // cout << "handwriting recognition wasm module loaded" << endl;
+
+    return 0;
+}
